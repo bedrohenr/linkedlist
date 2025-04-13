fa444b5c7e01b6640a8afc69feebe98da866b1c7
bedrohenr <ferfonhp@gmail.com>
2025-04-13 20:33:45 -0300

    utiliando linked list com alunos

--- Principal.java
+++ Principal.java
@@ -2,16 +2,21 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("-- Started");
 
-        LinkedList<Integer> ll = new LinkedList<>(); 
 
-        ll.add(12);
-        ll.add(24);
-        ll.add(36);
-        ll.add(48);
+        Aluno pedro = new Aluno(1, "pedro", 10);
+        Aluno kaio = new Aluno(2, "kaio", 9);
+        Aluno lucas = new Aluno(4, "lucas", 9);
+        Aluno alicia = new Aluno(3, "alicia", 9);
+
+        LinkedList<Aluno> ll = new LinkedList<>(false); 
+
+        ll.add(pedro);
+        ll.add(alicia);
+        ll.add(lucas);
+        ll.add(kaio);
 
         System.out.println(ll.toString());
         System.out.println("Tamanho da lista: " + ll.getSize());
-
         System.out.println("Ended --");
     }
     
