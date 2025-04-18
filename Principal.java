f2666c2f1fd0c9d8b992201a02f3c0eaa0c911e2
bedrohenr <ferfonhp@gmail.com>
2025-04-18 20:46:08 -0300

    classe principal para testes

--- Principal.java
+++ Principal.java
@@ -7,15 +7,31 @@ public class Principal {
         Aluno kaio = new Aluno(2, "kaio", 9);
         Aluno lucas = new Aluno(4, "lucas", 9);
         Aluno alicia = new Aluno(3, "alicia", 9);
+        Aluno marcos = new Aluno(2, "marcos", 9);
+        Aluno livia = new Aluno(4, "livia", 9);
+        Aluno andre = new Aluno(1, "andre", 9);
+        Aluno luiz = new Aluno(5, "luiz", 9);
+        Aluno jair = new Aluno(3, "jair", 9);
+        Aluno maome = new Aluno(3, "maome", 9);
+        Aluno denis = new Aluno(9, "denis", 9);
 
-        LinkedList<Aluno> ll = new LinkedList<>(false); 
+        LinkedList<Aluno> ll = new LinkedList<>(true); 
 
-        ll.add(pedro);
-        ll.add(alicia);
         ll.add(lucas);
+        ll.add(alicia);
+        ll.add(pedro);
         ll.add(kaio);
+        ll.add(andre);
+        ll.add(livia);
+        ll.add(marcos);
+        ll.add(jair);
+        ll.add(maome);
+        ll.add(luiz);
+        ll.add(denis);
 
         System.out.println(ll.toString());
+
+        // System.out.println("Teste" + pedro.compare(kaio));
         System.out.println("Tamanho da lista: " + ll.getSize());
         System.out.println("Ended --");
     }
