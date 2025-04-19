5bc2439af0396b9b4097b2bb7735bfe23eb29bf5
Pedro Ferreira <ferfonhp@gmail.com>
2025-04-19 14:51:02 -0300

    leitor com num aleatorio

--- LeitorArquivos.java
+++ LeitorArquivos.java
@@ -33,22 +33,29 @@ public class LeitorArquivos {
             int numRegistros = Integer.parseInt(reader.readLine().trim());
             System.out.println("Número de registros: " + numRegistros);
             System.out.println(); 
+
             String linha;
+
+            String[] partes; // Info da linha
+            int id;  // id/Matricua
+            String nome; // Nome do aluno
+            float nota;
+            Aluno aluno; // Obj aluno
+            System.out.println("Iniciando criação dos alunos pelo arquivo.\n...");
             while ((linha = reader.readLine()) != null) {
-                String[] partes = linha.split(";");
+                partes = linha.split(";");
                 // int id = Integer.parseInt(partes[0]);
-                int id = random.nextInt(numRegistros);
-                String nome = partes[1];
-                float nota = Float.parseFloat(partes[2]);
+                id = random.nextInt(numRegistros) +1; // Prevenindo matricula 0
+                nome = partes[1];
+                nota = Float.parseFloat(partes[2]);
                 
                 // System.out.printf("ID: %d | Nome: %s | Nota: %.2f%n", id, nome, nota);
-                Aluno aluno = new Aluno(id, nome, nota);
+                aluno = new Aluno(id, nome, nota);
 
                 sortedLL.add(aluno);
                 notSortedLL.add(aluno);
-
-                System.out.print("+");
             }
+            System.out.println("Fim da criação dos alunos e adicionados as listas.");
         } catch (IOException e) {
             System.err.println("Erro ao ler o arquivo: " + e.getMessage());
         } catch (NumberFormatException e) {
